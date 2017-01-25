4c3dfee3071674054bd44a4a2da5299380df0dff
Paul Ciarlo <paul.ciarlo@gmail.com>
2017-01-25 10:18:30 -0500

    move bash_profile options to file and source it from the main one

--- hack.sh
+++ hack.sh
@@ -209,5 +209,4 @@ echo "Kill affected applications"
 for app in Safari Finder Dock Mail SystemUIServer; do killall "$app" >/dev/null 2>&1; done
 
 echo "" >> ~/.bash_profile
-echo "export CLICOLOR=1" >> ~/.bash_profile
-echo "export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx" >> ~/.bash_profile
+echo ". $PWD/.bash_profile" >> ~/.bash_profile
